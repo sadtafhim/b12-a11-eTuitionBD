c3f6fe2266a9bb5c7e81647a41a159434b1a1ea2
Saad <tafhim.sad@gmail.com>
2025-12-16 00:03:13 +0600

    path secured

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,48 @@
 import axios from "axios";
-import React from "react";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
-const axiosSecure = axios.create({ baseURL: "http://localhost:3000" });
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:3000",
+});
 
 const useAxiosSecure = () => {
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const reqInterceptor = axiosSecure.interceptors.request.use(
+      async (config) => {
+        if (user) {
+          const token = await user.getIdToken();
+          config.headers.authorization = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (res) => res,
+      async (err) => {
+        const statusCode = err.response?.status;
+
+        if (statusCode === 401 || statusCode === 403) {
+          await logOut();
+          navigate("/auth/login");
+        }
+
+        return Promise.reject(err);
+      }
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, logOut, navigate]);
+
   return axiosSecure;
 };
 
